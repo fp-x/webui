ede2ffaf421c3d512ac22fcf23f113541319ef1b
Justin Laue <justin@fp-x.com>
2015-11-17 08:52:34 -0700

    more testing

--- source/Styles/xb3/code/includes/header.php
+++ source/Styles/xb3/code/includes/header.php
@@ -32,7 +32,7 @@
     
 	/* demo flag in session */
 	if (!isset($_SESSION['_DEBUG'])) {
-		$_DEBUG = file_exists('/var/ui_dev_debug');
+		$_DEBUG = file_exists('/fss/gw/var/ui_dev_debug');
 		$_SESSION['_DEBUG'] = $_DEBUG;
 	}
 	else {
